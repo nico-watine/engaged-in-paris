0a01eb3a9df8804e1a26af9f1934e6f895cfea1a
Nico Watine <nico@studioncreations.com>
2017-01-02 17:03:35 -0600

    Copied over all remaining pages
    Delete engaged.foundation repo !!

---
+++ src/pages/chapel-exit.php
@@ -0,0 +1,14 @@
+---
+url: /chapel-exit
+title: Chapel Exit
+description: Nico & Corinne bust out the moves for their First Dance
+og-title: "#teamWatine's First Dance"
+og-description: A couple's First Dance shouldn't leave guests yawning... Nico & Corinne had their guests hoopin' and hollerin'!
+og-image:
+body-id: chapel-exit
+body-class:
+---
+<section class="splash-landing">
+	<h1 class="heading-bold">Chapel Exit</h1>
+	<img src="{{ cdn }}/img/chapel-exit/splash-landing/1920w.jpg" alt="Nico &amp; Corinne's Chapel-Tunnel Exit'">
+</section>
