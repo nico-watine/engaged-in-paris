546ae76b43d2e6bf9f8b7b9b9244c41228da14c9
Nico Watine <nico@studioncreations.com>
2017-01-08 21:56:31 -0600

    Correctly referenced final photos in photoGallery

--- src/pages/the-send-off.php
+++ src/pages/the-send-off.php
@@ -8,52 +8,30 @@ og-image:
 body-id: the-send-off
 body-class: the-send-off
 ---
-<section class="splash-landing textShadow-black">
+<section class="splash-landing textShadow-black" style="margin-bottom: 70px;">
 	<h1 class="heading-bold">The Send-Off</h1>
 	<img src="{{ cdn }}/img/the-send-off/splash/1920w.jpg" alt="Nico &amp; Corinne send-off">
 </section>
 <section class="photoGallery">
 	<div class="row">
-		<div class="medium-4 columns">
-			<img srcset="{{ cdn }}/img/first-dance/gallery/1_2x.jpg 2x" src="{{ cdn }}/img/first-dance/gallery/1.jpg" alt="Nico &amp; Corinne's First Dance">
+		<div class="medium-6 columns">
+			<img srcset="/img/the-send-off/gallery/1_2x.jpg 2x" src="/img/the-send-off/gallery/1.jpg" alt="Nico &amp; Corinne's send-off">
 		</div>
-		<div class="medium-4 columns">
-			<img srcset="{{ cdn }}/img/first-dance/gallery/2_2x.jpg 2x" src="{{ cdn }}/img/first-dance/gallery/2.jpg" alt="Nico &amp; Corinne's First Dance">
-		</div>
-		<div class="medium-4 columns">
-			<img srcset="{{ cdn }}/img/first-dance/gallery/3_2x.jpg 2x" src="{{ cdn }}/img/first-dance/gallery/3.jpg" alt="Nico &amp; Corinne's First Dance">
+		<div class="medium-6 columns">
+			<img srcset="/img/the-send-off/gallery/2_2x.jpg 2x" src="/img/the-send-off/gallery/2.jpg" alt="Nico &amp; Corinne's send-off">
 		</div>
 	</div>
 	<div class="row">
-		<div class="medium-10 medium-push-1 large-8 large-push-2 columns">
-			<img srcset="{{ cdn }}/img/first-dance/gallery/10_2x.jpg 2x" src="{{ cdn }}/img/first-dance/gallery/10.jpg" alt="Nico &amp; Corinne's First Dance">
+		<div class="medium-6 medium-push-2 columns end">
+			<img srcset="/img/the-send-off/gallery/3_2x.jpg 2x" src="/img/the-send-off/gallery/3.jpg" alt="">
 		</div>
 	</div>
 	<div class="row">
-		<div class="medium-4 columns">
-			<img srcset="{{ cdn }}/img/first-dance/gallery/4_2x.jpg 2x" src="{{ cdn }}/img/first-dance/gallery/4.jpg" alt="Nico &amp; Corinne's First Dance">
+		<div class="medium-6 columns">
+			<img srcset="/img/the-send-off/gallery/4_2x.jpg 2x" src="/img/the-send-off/gallery/4.jpg" alt="Nico &amp; Corinne's send-off">
 		</div>
-		<div class="medium-4 columns">
-			<img srcset="{{ cdn }}/img/first-dance/gallery/5_2x.jpg 2x" src="{{ cdn }}/img/first-dance/gallery/5.jpg" alt="Nico &amp; Corinne's First Dance">
-		</div>
-		<div class="medium-4 columns">
-			<img srcset="{{ cdn }}/img/first-dance/gallery/6_2x.jpg 2x" src="{{ cdn }}/img/first-dance/gallery/6.jpg" alt="Nico &amp; Corinne's First Dance">
-		</div>
-	</div>
-	<div class="row">
-		<div class="medium-10 medium-push-1 large-8 large-push-2 columns">
-			<img srcset="{{ cdn }}/img/first-dance/gallery/9_2x.jpg 2x" src="{{ cdn }}/img/first-dance/gallery/9.jpg" alt="Nico &amp; Corinne's First Dance">
-		</div>
-	</div>
-	<div class="row">
-		<div class="medium-4 columns">
-			<img srcset="{{ cdn }}/img/first-dance/gallery/7_2x.jpg 2x" src="{{ cdn }}/img/first-dance/gallery/7.jpg" alt="Nico &amp; Corinne's First Dance">
-		</div>
-		<div class="medium-4 columns">
-			<img srcset="{{ cdn }}/img/first-dance/gallery/8_2x.jpg 2x" src="{{ cdn }}/img/first-dance/gallery/8.jpg" alt="Nico &amp; Corinne's First Dance">
-		</div>
-		<div class="medium-4 columns">
-			<img srcset="{{ cdn }}/img/first-dance/gallery/11_2x.jpg 2x" src="{{ cdn }}/img/first-dance/gallery/11.jpg" alt="Nico &amp; Corinne's First Dance">
+		<div class="medium-6 columns">
+			<img srcset="/img/the-send-off/gallery/5_2x.jpg 2x" src="/img/the-send-off/gallery/5.jpg" alt="Nico &amp; Corinne's send-off">
 		</div>
 	</div>
 </section>
