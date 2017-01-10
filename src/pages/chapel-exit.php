a32fdc63f6df539c477c440bde9cd98437ba29f0
Nico Watine <nico@studioncreations.com>
2017-01-10 02:34:31 -0600

    Update YAML head info
    Removed the first-dance entries
    Come back to correctly enter these values

--- src/pages/chapel-exit.php
+++ src/pages/chapel-exit.php
@@ -1,9 +1,9 @@
 ---
 url: /chapel-exit
-title: Chapel Exit
-description: Nico & Corinne bust out the moves for their First Dance
-og-title: "#teamWatine's First Dance"
-og-description: A couple's First Dance shouldn't leave guests yawning... Nico & Corinne had their guests hoopin' and hollerin'!
+title: Chapel-Tunnel Exit
+description: 
+og-title: "#teamWatine's Chapel-Tunnel Exit"
+og-description: 
 og-image:
 body-id: chapel-exit
 body-class:
