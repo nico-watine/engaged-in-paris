3e143003d29d9049c2846720fcd9f0a26cd76ca1
Nico Watine <nico@studioncreations.com>
2017-01-10 02:33:58 -0600

    Update splash-landing with correct classes

--- src/pages/chapel-exit.php
+++ src/pages/chapel-exit.php
@@ -8,7 +8,7 @@ og-image:
 body-id: chapel-exit
 body-class:
 ---
-<section class="splash-landing">
+<section class="splash-landing textShadow-black">
 	<h1 class="heading-bold">Chapel Exit</h1>
-	<img src="{{ cdn }}/img/chapel-exit/splash-landing/1920w.jpg" alt="Nico &amp; Corinne's Chapel-Tunnel Exit'">
+	<img src="{{ cdn }}/img/chapel-exit/splash-landing/1920w.jpg" alt="Nico &amp; Corinne's Chapel-Tunnel Exit">
 </section>
