85bed90d00242850a5dd0e1c3ae098cd5964d6e6
Nico Watine <nico@studioncreations.com>
2017-02-20 21:04:23 -0600

    Triple- .row(s) done

--- src/pages/first-look.php
+++ src/pages/first-look.php
@@ -8,63 +8,105 @@ og-image:
 body-id: first-look
 body-class: first-look
 ---
-<section class="splash-landing textShadow-black">
+<section class="splash-landing textShadow-black" style="margin-bottom: 70px;">
 	<h1 class="heading-bold">First Look</h1>
 	<img src="{{ cdn }}/img/first-look/splash/1920w.jpg" alt="Nico &amp; Corinne's First Look'">
 </section>
 <section class="photoGallery">
 	<div class="row">
-		<div class="medium-4 columns"></div>
-		<div class="medium-4 columns"></div>
-		<div class="medium-4 columns"></div>
+		<div class="medium-4 columns">
+			<img srcset="/img/first-look/gallery/1/1_2x.jpg 2x" src="/img/first-look/gallery/1/1.jpg" alt="Nico &amp; Corinne's First Look">
+		</div>
+		<div class="medium-4 columns">
+			<img srcset="/img/first-look/gallery/1/2_2x.jpg 2x" src="/img/first-look/gallery/1/2.jpg" alt="Nico &amp; Corinne's First Look">
+		</div>
+		<div class="medium-4 columns">
+			<img srcset="/img/first-look/gallery/1/3_2x.jpg 2x" src="/img/first-look/gallery/1/3.jpg" alt="Nico &amp; Corinne's First Look">
+		</div>
 	</div>
 	<div class="row">
 		<div class="medium-10 medium-push-1 large-8 large-push-2 columns"></div>
 	</div>
 	<div class="row">
-		<div class="medium-4 columns"></div>
-		<div class="medium-4 columns"></div>
-		<div class="medium-4 columns"></div>
+		<div class="medium-4 columns">
+			<img srcset="/img/first-look/gallery/3/1_2x.jpg 2x" src="/img/first-look/gallery/3/1.jpg" alt="Nico &amp; Corinne's First Look">
+		</div>
+		<div class="medium-4 columns">
+			<img srcset="/img/first-look/gallery/3/2_2x.jpg 2x" src="/img/first-look/gallery/3/2.jpg" alt="Nico &amp; Corinne's First Look">
+		</div>
+		<div class="medium-4 columns">
+			<img srcset="/img/first-look/gallery/3/3_2x.jpg 2x" src="/img/first-look/gallery/3/3.jpg" alt="Nico &amp; Corinne's First Look">
+		</div>
 	</div>
 	<div class="row">
 		<div class="medium-10 medium-push-1 large-8 large-push-2 columns"></div>
 	</div>
 	<div class="row">
-		<div class="medium-4 columns"></div>
-		<div class="medium-4 columns"></div>
-		<div class="medium-4 columns"></div>
+		<div class="medium-4 columns">
+			<img srcset="/img/first-look/gallery/5/1_2x.jpg 2x" src="/img/first-look/gallery/5/1.jpg" alt="Nico &amp; Corinne's First Look">
+		</div>
+		<div class="medium-4 columns">
+			<img srcset="/img/first-look/gallery/5/2_2x.jpg 2x" src="/img/first-look/gallery/5/2.jpg" alt="Nico &amp; Corinne's First Look">
+		</div>
+		<div class="medium-4 columns">
+			<img srcset="/img/first-look/gallery/5/3_2x.jpg 2x" src="/img/first-look/gallery/5/3.jpg" alt="Nico &amp; Corinne's First Look">
+		</div>
 	</div>
 	<div class="row">
 		<div class="medium-10 medium-push-1 large-8 large-push-2 columns"></div>
 	</div>
 	<div class="row">
-		<div class="medium-4 columns"></div>
-		<div class="medium-4 columns"></div>
-		<div class="medium-4 columns"></div>
+		<div class="medium-4 columns">
+			<img srcset="/img/first-look/gallery/7/1_2x.jpg 2x" src="/img/first-look/gallery/7/1.jpg" alt="Nico &amp; Corinne's First Look">
+		</div>
+		<div class="medium-4 columns">
+			<img srcset="/img/first-look/gallery/7/2_2x.jpg 2x" src="/img/first-look/gallery/7/2.jpg" alt="Nico &amp; Corinne's First Look">
+		</div>
+		<div class="medium-4 columns">
+			<img srcset="/img/first-look/gallery/7/3_2x.jpg 2x" src="/img/first-look/gallery/7/3.jpg" alt="Nico &amp; Corinne's First Look">
+		</div>
 	</div>
 	<div class="row">
 		<div class="medium-10 medium-push-1 large-8 large-push-2 columns"></div>
 	</div>
 	<div class="row">
-		<div class="medium-4 columns"></div>
-		<div class="medium-4 columns"></div>
-		<div class="medium-4 columns"></div>
+		<div class="medium-4 columns">
+			<img srcset="/img/first-look/gallery/9/1_2x.jpg 2x" src="/img/first-look/gallery/9/1.jpg" alt="Nico &amp; Corinne's First Look">
+		</div>
+		<div class="medium-4 columns">
+			<img srcset="/img/first-look/gallery/9/2_2x.jpg 2x" src="/img/first-look/gallery/9/2.jpg" alt="Nico &amp; Corinne's First Look">
+		</div>
+		<div class="medium-4 columns">
+			<img srcset="/img/first-look/gallery/9/3_2x.jpg 2x" src="/img/first-look/gallery/9/3.jpg" alt="Nico &amp; Corinne's First Look">
+		</div>
 	</div>
 	<div class="row">
 		<div class="medium-10 medium-push-1 large-8 large-push-2 columns"></div>
 	</div>
 	<div class="row">
-		<div class="medium-4 columns"></div>
-		<div class="medium-4 columns"></div>
-		<div class="medium-4 columns"></div>
+		<div class="medium-4 columns">
+			<img srcset="/img/first-look/gallery/11/1_2x.jpg 2x" src="/img/first-look/gallery/11/1.jpg" alt="Nico &amp; Corinne's First Look">
+		</div>
+		<div class="medium-4 columns">
+			<img srcset="/img/first-look/gallery/11/2_2x.jpg 2x" src="/img/first-look/gallery/11/2.jpg" alt="Nico &amp; Corinne's First Look">
+		</div>
+		<div class="medium-4 columns">
+			<img srcset="/img/first-look/gallery/11/3_2x.jpg 2x" src="/img/first-look/gallery/11/3.jpg" alt="Nico &amp; Corinne's First Look">
+		</div>
 	</div>
 	<div class="row">
 		<div class="medium-10 medium-push-1 large-8 large-push-2 columns"></div>
 	</div>
 	<div class="row">
-		<div class="medium-4 columns"></div>
-		<div class="medium-4 columns"></div>
-		<div class="medium-4 columns"></div>
+		<div class="medium-4 columns">
+			<img srcset="/img/first-look/gallery/13/1_2x.jpg 2x" src="/img/first-look/gallery/13/1.jpg" alt="Nico &amp; Corinne's First Look">
+		</div>
+		<div class="medium-4 columns">
+			<img srcset="/img/first-look/gallery/13/2_2x.jpg 2x" src="/img/first-look/gallery/13/2.jpg" alt="Nico &amp; Corinne's First Look">
+		</div>
+		<div class="medium-4 columns">
+			<img srcset="/img/first-look/gallery/13/3_2x.jpg 2x" src="/img/first-look/gallery/13/3.jpg" alt="Nico &amp; Corinne's First Look">
+		</div>
 	</div>
 	<div class="row">
 		<div class="medium-10 medium-push-1 large-8 large-push-2 columns"></div>
