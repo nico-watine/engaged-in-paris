5c6f8f43ae793857c33c3f529e37dbb2b16865f6
Nico Watine <nico@studioncreations.com>
2017-02-20 20:41:44 -0600

    first-look page setup

---
+++ src/pages/first-look.php
@@ -0,0 +1,14 @@
+---
+url: /first-look
+title: First Look
+description: 
+og-title: "#teamWatine's First Look"
+og-description: 
+og-image:
+body-id: first-look
+body-class: first-look
+---
+<section class="splash-landing textShadow-black">
+	<h1 class="heading-bold">First Look</h1>
+	<img src="" alt="Nico &amp; Corinne's First Look'">
+</section>
